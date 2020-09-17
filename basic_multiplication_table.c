97abf00b9b177dd618aa5e9d369fc16e2fd4711a
Bipul Mukherjee <58104528+bipuladict@users.noreply.github.com>
2020-09-17 12:47:45 +0530

    Create basic_multiplication_table.c

---
+++ basic_multiplication_table.c
@@ -0,0 +1,50 @@
+#include<stdio.h>
+int main()
+
+{
+	int i,n,k;
+	printf("ENTER THE TABLE NUMBER ---->\n");
+	scanf("%d",&n);
+	printf("********THE TABLE IS*********\n");
+	for(i=1;i<=10;i++)
+	{
+		k = n*i;
+		
+		printf("%d X %d = %d \n",n,i,k);
+		
+	
+	
+	
+	
+	}
+
+
+	return 0;
+
+
+
+
+}
+/*              
+
+ENTER THE TABLE NUMBER ---->
+4
+********THE TABLE IS*********
+4 X 1 = 4
+4 X 2 = 8
+4 X 3 = 12
+4 X 4 = 16
+4 X 5 = 20
+4 X 6 = 24
+4 X 7 = 28
+4 X 8 = 32
+4 X 9 = 36
+4 X 10 = 40
+
+
+
+
+
+
+
+*\
